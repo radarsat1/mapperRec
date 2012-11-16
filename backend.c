a4ab9dd841a94ca844f9bef90a9a1e6c56fdd30f
Stephen Sinclair <sinclair@music.mcgill.ca>
2012-11-15 21:08:56 -0500

    Initialize function pointers to zero.

--- backend.c
+++ backend.c
@@ -5,7 +5,7 @@ const char *backend_strings[N_BACKENDS] = { "file", "binary", "oscstreamdb" };
 
 backends_t backend = BACKEND_FILE;
 
-int (*backend_start)();
-void (*backend_stop)();
-int (*backend_poll)();
-void (*backend_write_value)(mapper_signal msig, void *v);
+int (*backend_start)() = 0;
+void (*backend_stop)() = 0;
+int (*backend_poll)() = 0;
+void (*backend_write_value)(mapper_signal msig, void *v) = 0;
